16c718e3f031d68b5b400a4d2512f4dfbd95ef9b
root <lorison.gilles@gmail.com>
2021-02-07 18:58:53 -0300

    primary-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './home/gilles30/www/Project/curso-angular/my-component/my-component.component';
+import { MyComponent1Component } from './home/gilles30/www/Project/curso-angular/primeiro-projeto/src/my-component1/my-component1.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      MyComponentComponent,
+      MyComponent1Component
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
