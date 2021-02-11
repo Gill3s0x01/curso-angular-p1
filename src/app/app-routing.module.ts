c1e06b967624586b29772f6ed40f91464fef3690
root <lorison.gilles@gmail.com>
2021-02-10 22:55:24 -0300

    aula8a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AppModule } from './app.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -5,6 +6,6 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, AppModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
